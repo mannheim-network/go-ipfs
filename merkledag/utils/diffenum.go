ipfs/kubo

implement ipfs pin update
License: MIT
Signed-off-by: Jeromy <jeromyj@gmail.com>

merkledag/utils/diffenum.go
@@ -1,0 +1,75 @@
+package dagutils
+
+import (
+	"context"
+	"fmt"
+
+	mdag "github.com/ipfs/go-ipfs/merkledag"
+
+	cid "gx/ipfs/QmV5gPoRsjN1Gid3LMdNZTyfCtP2DsvqEbMAmz82RmmiGk/go-cid"
+	node "gx/ipfs/QmYDscK7dmdo2GZ9aumS8s5auUUAH5mR1jvj5pYhWusfK7/go-ipld-node"
+)
+
+// DiffEnumerate fetches every object in the graph pointed to by 'to' that is
+// not in 'from'. This can be used to more efficiently fetch a graph if you can
+// guarantee you already have the entirety of 'from'
+func DiffEnumerate(ctx context.Context, dserv node.NodeGetter, from, to *cid.Cid) error {
+	fnd, err := dserv.Get(ctx, from)
+	if err != nil {
+		return fmt.Errorf("get %s: %s", from, err)
+	}
+
+	tnd, err := dserv.Get(ctx, to)
+	if err != nil {
+		return fmt.Errorf("get %s: %s", to, err)
+	}
+
+	diff := getLinkDiff(fnd, tnd)
+
+	sset := cid.NewSet()
+	for _, c := range diff {
+		if c.a == nil {
+			err := mdag.EnumerateChildrenAsync(ctx, mdag.GetLinksDirect(dserv), c.b, sset.Visit)
+			if err != nil {
+				return err
+			}
+		} else {
+			err := DiffEnumerate(ctx, dserv, c.a, c.b)
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
+}
+
+type diffpair struct {
+	a, b *cid.Cid
+}
+
+func getLinkDiff(a, b node.Node) []diffpair {
+	have := make(map[string]*node.Link)
+	names := make(map[string]*node.Link)
+	for _, l := range a.Links() {
+		have[l.Cid.KeyString()] = l
+		names[l.Name] = l
+	}
+
+	var out []diffpair
+
+	for _, l := range b.Links() {
+		if have[l.Cid.KeyString()] != nil {
+			continue
+		}
+
+		match, ok := names[l.Name]
+		if !ok {
+			out = append(out, diffpair{b: l.Cid})
+			continue
+		}
+
+		out = append(out, diffpair{a: match.Cid, b: l.Cid})
+	}
+	return out
+}

merkledag/utils/diffenum_test.go
@@ -1,0 +1,149 @@
+package dagutils
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	dag "github.com/ipfs/go-ipfs/merkledag"
+	mdtest "github.com/ipfs/go-ipfs/merkledag/test"
+
+	cid "gx/ipfs/QmV5gPoRsjN1Gid3LMdNZTyfCtP2DsvqEbMAmz82RmmiGk/go-cid"
+	node "gx/ipfs/QmYDscK7dmdo2GZ9aumS8s5auUUAH5mR1jvj5pYhWusfK7/go-ipld-node"
+)
+
+func buildNode(name string, desc map[string]ndesc, out map[string]node.Node) node.Node {
+	this := desc[name]
+	nd := new(dag.ProtoNode)
+	nd.SetData([]byte(name))
+	for k, v := range this {
+		child, ok := out[v]
+		if !ok {
+			child = buildNode(v, desc, out)
+			out[v] = child
+		}
+
+		if err := nd.AddNodeLink(k, child); err != nil {
+			panic(err)
+		}
+	}
+
+	return nd
+}
+
+type ndesc map[string]string
+
+func mkGraph(desc map[string]ndesc) map[string]node.Node {
+	out := make(map[string]node.Node)
+	for name := range desc {
+		if _, ok := out[name]; ok {
+			continue
+		}
+
+		out[name] = buildNode(name, desc, out)
+	}
+	return out
+}
+
+var tg1 = map[string]ndesc{
+	"a1": ndesc{
+		"foo": "b",
+	},
+	"b": ndesc{},
+	"a2": ndesc{
+		"foo": "b",
+		"bar": "c",
+	},
+	"c": ndesc{},
+}
+
+var tg2 = map[string]ndesc{
+	"a1": ndesc{
+		"foo": "b",
+	},
+	"b": ndesc{},
+	"a2": ndesc{
+		"foo": "b",
+		"bar": "c",
+	},
+	"c": ndesc{"baz": "d"},
+	"d": ndesc{},
+}
+
+func TestDiffEnumBasic(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	nds := mkGraph(tg1)
+
+	ds := mdtest.Mock()
+	lgds := &getLogger{ds: ds}
+
+	for _, nd := range nds {
+		_, err := ds.Add(nd)
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	err := DiffEnumerate(ctx, lgds, nds["a1"].Cid(), nds["a2"].Cid())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = assertCidList(lgds.log, []*cid.Cid{nds["a1"].Cid(), nds["a2"].Cid(), nds["c"].Cid()})
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+type getLogger struct {
+	ds  node.NodeGetter
+	log []*cid.Cid
+}
+
+func (gl *getLogger) Get(ctx context.Context, c *cid.Cid) (node.Node, error) {
+	nd, err := gl.ds.Get(ctx, c)
+	if err != nil {
+		return nil, err
+	}
+	gl.log = append(gl.log, c)
+	return nd, nil
+}
+
+func assertCidList(a, b []*cid.Cid) error {
+	if len(a) != len(b) {
+		return fmt.Errorf("got different number of cids than expected")
+	}
+	for i, c := range a {
+		if !c.Equals(b[i]) {
+			return fmt.Errorf("expected %s, got %s", c, b[i])
+		}
+	}
+	return nil
+}
+func TestDiffEnumFail(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	nds := mkGraph(tg2)
+
+	ds := mdtest.Mock()
+	lgds := &getLogger{ds: ds}
+
+	for _, s := range []string{"a1", "a2", "b", "c"} {
+		_, err := ds.Add(nds[s])
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	err := DiffEnumerate(ctx, lgds, nds["a1"].Cid(), nds["a2"].Cid())
+	if err != dag.ErrNotFound {
+		t.Fatal("expected err not found")
+	}
+
+	err = assertCidList(lgds.log, []*cid.Cid{nds["a1"].Cid(), nds["a2"].Cid(), nds["c"].Cid()})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+}

mannheim-network/go-ipfs

merkledag/utils/diffenum.go
@@ -1,0 +1,75 @@
+package dagutils
+
+import (
+	"context"
+	"fmt"
+
+	mdag "github.com/ipfs/go-ipfs/merkledag"
+
+	node "github.com/ipfs/go-ipld-node"
+	cid "gx/ipfs/QmYhQaCYEcaPPjxJX7YcPcVKkQfRy6sJ7B3XmGFk82XYdQ/go-cid"
+)
+
+// DiffEnumerate fetches every object in the graph pointed to by 'to' that is
+// not in 'from'. This can be used to more efficiently fetch a graph if you can
+// guarantee you already have the entirety of 'from'
+func DiffEnumerate(ctx context.Context, dserv node.NodeGetter, from, to *cid.Cid) error {
+	fnd, err := dserv.Get(ctx, from)
+	if err != nil {
+		return fmt.Errorf("get %s: %s", from, err)
+	}
+
+	tnd, err := dserv.Get(ctx, to)
+	if err != nil {
+		return fmt.Errorf("get %s: %s", to, err)
+	}
+
+	diff := getLinkDiff(fnd, tnd)
+
+	sset := cid.NewSet()
+	for _, c := range diff {
+		if c.a == nil {
+			err := mdag.EnumerateChildrenAsync(ctx, mdag.GetLinksDirect(dserv), c.b, sset.Visit)
+			if err != nil {
+				return err
+			}
+		} else {
+			err := DiffEnumerate(ctx, dserv, c.a, c.b)
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
+}
+
+type diffpair struct {
+	a, b *cid.Cid
+}
+
+func getLinkDiff(a, b node.Node) []diffpair {
+	have := make(map[string]*node.Link)
+	names := make(map[string]*node.Link)
+	for _, l := range a.Links() {
+		have[l.Cid.KeyString()] = l
+		names[l.Name] = l
+	}
+
+	var out []diffpair
+
+	for _, l := range b.Links() {
+		if have[l.Cid.KeyString()] != nil {
+			continue
+		}
+
+		match, ok := names[l.Name]
+		if !ok {
+			out = append(out, diffpair{b: l.Cid})
+			continue
+		}
+
+		out = append(out, diffpair{a: match.Cid, b: l.Cid})
+	}
+	return out
+}

merkledag/utils/diffenum_test.go
@@ -1,0 +1,149 @@
+package dagutils
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	dag "github.com/ipfs/go-ipfs/merkledag"
+	mdtest "github.com/ipfs/go-ipfs/merkledag/test"
+
+	node "github.com/ipfs/go-ipld-node"
+	cid "gx/ipfs/QmYhQaCYEcaPPjxJX7YcPcVKkQfRy6sJ7B3XmGFk82XYdQ/go-cid"
+)
+
+func buildNode(name string, desc map[string]ndesc, out map[string]node.Node) node.Node {
+	this := desc[name]
+	nd := new(dag.ProtoNode)
+	nd.SetData([]byte(name))
+	for k, v := range this {
+		child, ok := out[v]
+		if !ok {
+			child = buildNode(v, desc, out)
+			out[v] = child
+		}
+
+		if err := nd.AddNodeLink(k, child); err != nil {
+			panic(err)
+		}
+	}
+
+	return nd
+}
+
+type ndesc map[string]string
+
+func mkGraph(desc map[string]ndesc) map[string]node.Node {
+	out := make(map[string]node.Node)
+	for name := range desc {
+		if _, ok := out[name]; ok {
+			continue
+		}
+
+		out[name] = buildNode(name, desc, out)
+	}
+	return out
+}
+
+var tg1 = map[string]ndesc{
+	"a1": ndesc{
+		"foo": "b",
+	},
+	"b": ndesc{},
+	"a2": ndesc{
+		"foo": "b",
+		"bar": "c",
+	},
+	"c": ndesc{},
+}
+
+var tg2 = map[string]ndesc{
+	"a1": ndesc{
+		"foo": "b",
+	},
+	"b": ndesc{},
+	"a2": ndesc{
+		"foo": "b",
+		"bar": "c",
+	},
+	"c": ndesc{"baz": "d"},
+	"d": ndesc{},
+}
+
+func TestDiffEnumBasic(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	nds := mkGraph(tg1)
+
+	ds := mdtest.Mock()
+	lgds := &getLogger{ds: ds}
+
+	for _, nd := range nds {
+		_, err := ds.Add(nd)
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	err := DiffEnumerate(ctx, lgds, nds["a1"].Cid(), nds["a2"].Cid())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = assertCidList(lgds.log, []*cid.Cid{nds["a1"].Cid(), nds["a2"].Cid(), nds["c"].Cid()})
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+type getLogger struct {
+	ds  node.NodeGetter
+	log []*cid.Cid
+}
+
+func (gl *getLogger) Get(ctx context.Context, c *cid.Cid) (node.Node, error) {
+	nd, err := gl.ds.Get(ctx, c)
+	if err != nil {
+		return nil, err
+	}
+	gl.log = append(gl.log, c)
+	return nd, nil
+}
+
+func assertCidList(a, b []*cid.Cid) error {
+	if len(a) != len(b) {
+		return fmt.Errorf("got different number of cids than expected")
+	}
+	for i, c := range a {
+		if !c.Equals(b[i]) {
+			return fmt.Errorf("expected %s, got %s", c, b[i])
+		}
+	}
+	return nil
+}
+func TestDiffEnumFail(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	nds := mkGraph(tg2)
+
+	ds := mdtest.Mock()
+	lgds := &getLogger{ds: ds}
+
+	for _, s := range []string{"a1", "a2", "b", "c"} {
+		_, err := ds.Add(nds[s])
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	err := DiffEnumerate(ctx, lgds, nds["a1"].Cid(), nds["a2"].Cid())
+	if err != dag.ErrNotFound {
+		t.Fatal("expected err not found")
+	}
+
+	err = assertCidList(lgds.log, []*cid.Cid{nds["a1"].Cid(), nds["a2"].Cid(), nds["c"].Cid()})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+}